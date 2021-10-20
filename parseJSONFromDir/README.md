971366b72bfdc447156e881ccb89fef85a3b792d
M. Choji <mchoji@users.noreply.github.com>
2021-10-20 10:39:22 -0300

    Add tool parseJSONFromDir

---
+++ parseJSONFromDir/README.md
@@ -0,0 +1,43 @@
+# parseJSONFromDir.py
+
+Given a set of directories, look for files containing lists of objects in JSON format.
+Parse all of them and generate a pandas' DataFrame from their content, outputing
+the result in CSV format.
+
+## Use Case
+
+You've run Burp Intruder and saved server responses in given directories.
+Now you want to parse the results (lists of objects) to have the
+information organized (possibly dumps of something).
+
+## Help
+
+```shell
+poetry run ./parseJSONFromDir.py -h
+usage: parseJSONFromDir.py [-h] [-n] [-r] [-s] [--columns COLUMN [COLUMN ...]] [--drop-columns COLUMN [COLUMN ...]] [--sort-by COLUMN [COLUMN ...]] [--query QUERY]
+                           dir [dir ...]
+
+Parse JSON lists from files inside a directory
+
+positional arguments:
+  dir                   root directories
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n, --dry-run         Only list the files that should be read
+  -r, --recursive       Recursively search for files starting from root directory
+  -s, --silent          Ommit logging messages
+  --columns COLUMN [COLUMN ...]
+                        Columns to write to output
+  --drop-columns COLUMN [COLUMN ...]
+                        Drop columns (properties) from result
+  --sort-by COLUMN [COLUMN ...]
+                        Sort result by columns
+  --query QUERY         Apply query to filter final dataset
+```
+
+## Example
+
+```
+poetry run ./parseJSONFromDir.py /opt/dirA /opt/dirB --drop-columns X Y Z --sort-by A --columns A B C --query "D==True" -r > output.csv
+```
---
+++ parseJSONFromDir/parseJSONFromDir.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import argparse
+import json
+import logging
+import re
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser(description="Parse JSON lists from files inside a directory")
+parser.add_argument(
+        "dir",
+        help="root directories",
+        nargs='+',
+        )
+parser.add_argument(
+        "-n",
+        "--dry-run",
+        dest="dryrun",
+        help="Only list the files that should be read",
+        action="store_true",
+        )
+parser.add_argument(
+        "-r",
+        "--recursive",
+        help="Recursively search for files starting from root directory",
+        action="store_true",
+        )
+parser.add_argument(
+        "-s",
+        "--silent",
+        help="Ommit logging messages",
+        action="store_true",
+        )
+parser.add_argument(
+        "--columns",
+        nargs='+',
+        help="Columns to write to output",
+        dest="columns",
+        metavar="COLUMN"
+        )
+parser.add_argument(
+        "--drop-columns",
+        nargs='+',
+        help="Drop columns (properties) from result",
+        dest="dropc",
+        metavar="COLUMN",
+        )
+parser.add_argument(
+        "--sort-by",
+        nargs='+',
+        help="Sort result by columns",
+        dest="sortby",
+        metavar="COLUMN",
+        )
+parser.add_argument(
+        "--query",
+        type=str,
+        help="Apply query to filter final dataset",
+        )
+
+args = parser.parse_args()
+
+def search_files(directory, recursive=False):
+    dirpath = Path(directory)
+    assert dirpath.is_dir()
+    file_list = []
+    for x in dirpath.iterdir():
+        if x.is_file():
+            file_list.append(x)
+            logging.info("Found file %s", x)
+        else:
+            logging.info("Found directory %s", x)
+            if recursive:
+                file_list.extend(search_files(x))
+    return file_list
+
+
+if (args.silent):
+    logging.disable()
+
+logging.info("Searching files starting from %s", args.dir)
+
+file_list = []
+for dir in args.dir:
+    file_list.extend(search_files(dir, args.recursive))
+
+if args.dryrun:
+    logging.info("Running in dry-run mode, so finishing here")
+    exit(0)
+
+
+df_list = []
+row_count = 0
+for file in file_list:
+    content = file.read_text()
+    body = re.search(r'(\[.*\])', content)
+    if body:
+        try:
+            obj = json.loads(body.group(0))
+        except:
+            logging.error("Unable to parse content from %s as JSON: %s", file, body.group(0))
+        else:
+            df_list.append(pd.DataFrame.from_dict(obj))
+            #result_df = pd.concat([result_df, pd.DataFrame.from_dict(obj)], ignore_index=True)
+            row_count += df_list[-1].shape[0]
+            logging.info("Current row count: %d", row_count)
+
+if row_count == 0:
+    logging.warning("No data was parsed. Verify if files in directory contains lists of objects")
+else:
+    result_df = pd.concat(df_list, ignore_index=True)
+    logging.info("Content parsed. Removing duplicates...")
+    result_df.drop_duplicates(inplace=True)
+    if args.query:
+        logging.info("Applying query to dataset...")
+        result_df.query(args.query, inplace=True)
+    if args.sortby:
+        logging.info("Sorting data...")
+        result_df.sort_values(args.sortby, inplace=True)
+    if args.dropc:
+        logging.info("Dropping columns from result...")
+        result_df.drop(columns=args.dropc, inplace=True)
+    print(result_df.to_csv(index=False, columns=args.columns))
+
